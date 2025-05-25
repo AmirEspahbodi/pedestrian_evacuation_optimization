3344e5a8a89674fe394b0c4176a313869a8159d5
amir.espahbodi <dev.amir.espahbodi@gmail.com>
2025-05-25 07:07:37 +0200

    change folder structure

--- src/simulator/__init__.py
+++ src/simulator/__init__.py
@@ -1,15 +1,4 @@
-from .access import Access
-from .obstacle import Obstacle
-from .cellular_automata import CellularAutomata
-from .domain import Domain
-from .environment import Environment
-from .ca_state import CAState
+from .environment import __all__ as environment_all
+from .environment import Access, Obstacle, CellularAutomata, Domain, Environment, CAState
 
-__all__ = [
-    "Access",
-    "Obstacle",
-    "CellularAutomata",
-    "Domain",
-    "Environment",
-    "CAState",
-]
+__all__ = [*environment_all]
