f6efc3152d9b2df190582a96536dff1e5842f9cf
amir.espahbodi <dev.amir.espahbodi@gmail.com>
2025-08-01 15:04:15 +0200

    add timer to ea algorithm

--- src/run_ea.py
+++ src/run_ea.py
@@ -6,17 +6,19 @@ if __name__ == "__main__":
         "dataset/environments/environments_supermarket.json"
     )
     random_domain = [domain for domain in environment.domains if domain.id == 18][0]
-    emergency_accesses, fitness_value, history = evolutionary_algorithm(
+    emergency_accesses, fitness_value, history, time_to_best = evolutionary_algorithm(
         random_domain
     )
-    print(fitness_value)
-    print(emergency_accesses)
+    print(f"fitness_value = {fitness_value}")
+    print(f"emergency_accesses = {emergency_accesses}")
+    print(f"time_to_best = {time_to_best}")
     print(history)
+    
     with open("ea_hustory.txt", "w") as fp:
         fp.write(str(history))
     with open("ea_emergency_accesses.txt", "w") as fp:
         fp.write(
-            f"emergency_accesses={emergency_accesses}, fitness_value={fitness_value}"
+            f"emergency_accesses={emergency_accesses}, fitness_value={fitness_value}, time_to_best = {time_to_best}"
         )
 
     print("ea optimizing completed!")
